f29c03ad1d9d56f45552dd4b3e995921fba7ca6f
scw <kaya1436@daum.net>
2021-07-09 16:55:45 +0900

    httpresponse 적용

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse('<h1>안녕하세요.</h1>')
+    return HttpResponse('<h1>안녕하세요. </h1>')
