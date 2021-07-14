e747e242a8a5347582992236027d34c06b55c220
scw <kaya1436@daum.net>
2021-07-14 09:43:36 +0900

    delete config files

--- home/views.py
+++ home/views.py
@@ -3,4 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse('<h1>안녕하세요. </h1>')
+    path = request.path
+    resultstr = ''
+    if path == '/home':
+        resultstr = '<h1>여기는 home입니다.</h1>'
+    else:
+        resultstr = '<h1>여기는 main입니다.</h1>'
+
+    return HttpResponse(resultstr)
+
+def index01(request):
+    result = {'first':'cw', 'second':'shin'}
+    return render(request, 'index.html', context=result)
+
+def index02(request):
+    result = {'first':request.GET['first'], 'second':request.GET['second']}
+    return render(request, 'index.html', context=result)
